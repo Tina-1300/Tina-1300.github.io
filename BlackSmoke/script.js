3ffb414d8bbe93ef025ab7936fedbcb0efe7b63f
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2025-05-17 21:16:42 +0200

    initial commit

---
+++ BlackSmoke/script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    const body = document.querySelector('body');
+    body.style.opacity = '1';
+};
+
+// let backgroundSound = null;
+// function RunAudi(){
+//     if (!backgroundSound){
+//         backgroundSound = new Audio("LouisAttackJeTemeneAuVent.mp3");
+//         backgroundSound.loop = true;
+//         backgroundSound.play();
+//     }
+// };
+
+// document.addEventListener('click', function(){
+//     RunAudi();
+// });
